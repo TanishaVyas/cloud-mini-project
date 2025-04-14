25659580fbf56986409df59b0716a1c64c479ef1
Tanisha Vyas <122625790+TanishaVyas@users.noreply.github.com>
2025-04-14 18:41:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cloud-mini-project
+ 
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+        <div className="jenkins-text">
+      Cloud Mini Project
+      </div>
+      <div className="jenkins-text">
+      Jenkins & GitHub Integration for CI/CD Pipelines
+      </div>
+    </div>
+  );
+}
+
+export default App; 
